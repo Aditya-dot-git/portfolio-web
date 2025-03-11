816feda194e85edba95da5e58ac6d4f5c7a1ed74
Aditya Jha <adityajhajee7889@gmail.com>
2025-03-11 11:03:56 +0530

    Some Updates

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import "../styles/HeroSection.css";
 import { ThemeContext } from "./ThemeContext";
+import { FaLinkedin, FaGithub } from "react-icons/fa"; // Import icons
 
 const HeroSection = () => {
   const { theme } = useContext(ThemeContext);
@@ -22,6 +23,24 @@ const HeroSection = () => {
         <a href="mailto:adityajhajee7889@gmail.com" className="hero-btn">
           Get in Touch
         </a>
+        <div className="social-icons">
+          <a
+            href="https://linkedin.com/in/aditya-jha05" 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon-link"
+          >
+            <FaLinkedin className="social-icon linkedin" />
+          </a>
+          <a
+            href="https://github.com/adityajha28 "
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon-link"
+          >
+            <FaGithub className="social-icon github" />
+          </a>
+        </div>
       </div>
     </section>
   );
