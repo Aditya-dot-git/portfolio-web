91a4f11167613e8a9eee74bd6ee4a25a3a2a534f
Aditya Jha <adityajhajee7889@gmail.com>
2025-12-18 05:46:35 +0530

    good to go

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -36,7 +36,7 @@ const SkillsSection = () => {
 
   const skills = [
     {
-      category: "Programming Languages",
+      category: "Programming Language",
       items: [
         { name: "Java", icon: <FaJava /> },
         
